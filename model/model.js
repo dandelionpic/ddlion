e62dee062df7636c433c42a534d5f7ee7ae82bda
Ivan Zhang <ivanzhang410@gmail.com>
2019-03-20 23:43:01 -0400

    save comments data to mongodb by using mongoose

--- model/model.js
+++ model/model.js
@@ -6,7 +6,7 @@ mongoose.connect(DB_URL)
 
 const models = {
 	comments:{
-		'username':{type:String, 'require':true},
+		'name':{type:String, 'require':true},
 		'email':{type:String, 'require':true},
 		'message':{'type':String}
 	}
