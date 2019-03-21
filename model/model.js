33072ce53e51bab0f474ffc89fb9b8a343d9bc78
Ivan Zhang <ivanzhang410@gmail.com>
2019-03-20 20:10:11 -0400

    add model used to connect database

---
+++ model/model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const DB_URL = 'mongodb://localhost:27017/ddlion-comment'
+mongoose.connect(DB_URL)
+
+
+const models = {
+	comments:{
+		'username':{type:String, 'require':true},
+		'email':{type:String, 'require':true},
+		'message':{'type':String}
+	}
+}
+
+for(let m in models){
+	mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+	getModel:function(name){
+		return mongoose.model(name)
+	}
+}
+
+
